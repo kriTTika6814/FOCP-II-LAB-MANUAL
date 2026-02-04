dc90fad0506e9cc018588820950b530d653a6a34
Krittika Koundal <krittika25csu121@ncuindia.edu>
2026-02-04 11:12:14 +0530

    q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -3,7 +3,10 @@
 #include<iomanip>
 int main()
 {int marks1, marks2, marks3;
+ float average;
  std::cout<<"enter the marks of three students:";
  std::cin>>marks1>>marks2>>marks3;
- std::cout<<"1st student:"<<marks1<<std::endl<<"2nd student:"<<marks2<<std::endl<<"3rd student:"<<marks3;
+ average=(marks1+marks2+marks3)/3;
+ std::cout<<"1st student:"<<marks1<<std::endl<<"2nd student:"<<marks2<<std::endl<<"3rd student:"<<marks3<<std::endl<<"average:"<<average;
  return 0; }
+
